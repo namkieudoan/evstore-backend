86815893cc36d7e663f47d44224c71043a3f14a8
kieudoannam <kieunam2808@gmail.com>
2023-12-09 10:57:03 +0700

    create models: category, product, baseEntity

---
+++ src/main/java/com/project/shopapp/models/Product.java
@@ -0,0 +1,35 @@
+package com.project.shopapp.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data //toString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 350)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "thumbnail", length = 300)
+    private String thumbnail;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @Column(name = "category_id")
+    private Category categoryId;
+
+}
